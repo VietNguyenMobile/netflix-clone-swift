e58be561c67f5694950387241ed08c53f7a564e1
Viet Nguyen <>
2024-02-29 14:20:34 +0700

    set up basic Tabbar Navigation 4 screens

---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Macbook on 29/02/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -13,6 +13,13 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBlue
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
